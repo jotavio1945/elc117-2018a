7a848a873dc470e8574e23910648da31b37c97f8
jotavio1945 <jotavio1945@hotmail.com>
2018-03-15 23:35:14 -0300

    ex3 feito SEM LAMBDA

--- t1/t1.hs
+++ t1/t1.hs
@@ -7,3 +7,14 @@ aux2 :: String -> String
 aux2 elem = elem ++ ","
 addComma :: [String] -> [String]
 addComma elem = map aux2 elem
+
+--Crie uma função htmlListItems :: [String] -> [String], que receba uma lista de strings e retorne outra lista contendo as strings formatadas como itens de lista em HTML. Resolva este exercício COM e SEM funções anônimas (lambda).
+
+--semLAMBDA
+aux3 :: String -> String
+aux3 elem = "<LI>" ++ elem ++ "</LI>"
+
+htmlListItems :: [String] -> [String]
+htmlListItems elem = map aux3 elem
+
+--comLAMBDA
