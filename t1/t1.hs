0181543b0fdc817cda33e4376d414716af1e712a
jotavio1945 <jotavio1945@hotmail.com>
2018-03-15 22:25:48 -0300

    ex2 feito
    exercicio 2 foi feito, funciona.

--- t1/t1.hs
+++ t1/t1.hs
@@ -2,3 +2,8 @@
 isVowel :: Char -> Bool
 isVowel c =  c == 'a' ||c== 'e' ||c== 'i' ||c== 'o' ||c== 'u'
 
+--Escreva uma função addComma, que adicione uma vírgula no final de cada string contida numa lista.
+aux2 :: String -> String
+aux2 elem = elem ++ ","
+addComma :: [String] -> [String]
+addComma elem = map aux2 elem
